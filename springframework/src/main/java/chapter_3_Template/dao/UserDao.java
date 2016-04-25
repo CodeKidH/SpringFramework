1071bed812cb2a1a3796f97110e3b2a9d7241c9a
JeongHee <jeongh389@gmail.com>
2016-04-25 15:26:24 +0900

    Callback

--- springframework/src/main/java/chapter_3_Template/dao/UserDao.java
+++ springframework/src/main/java/chapter_3_Template/dao/UserDao.java
@@ -108,16 +108,18 @@ public class UserDao {
 	
 	public void deleteAll() throws SQLException{
 		
+		executeSql("delete from users");
+		
+	}
+	
+	private void executeSql(final String query)throws SQLException{
 		this.jdbcContext.workWithStatementStrategy(
-				
 				new StatementStrategy(){
-						public PreparedStatement makePreparedStatement(Connection c)throws SQLException{
-							return c.prepareStatement("delete from users");
-						}
-							
+					public PreparedStatement makePreparedStatement(Connection c)throws SQLException{
+						return c.prepareStatement(query);
+					}
 				}
 		);
-		
 	}
 	
 	public int getCount() throws SQLException{
